828481649234b8756b3808296428087d305833a2
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2023-06-10 16:24:40 +0600

    try to reproduce issue

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -4,7 +4,32 @@ import shared
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        Main_iosKt.MainViewController()
+        let tabs = UITabBarController()
+        
+        let tab1 = UINavigationController(
+            rootViewController: NativeUI()
+        )
+        tab1.tabBarItem = UITabBarItem(
+            title: "tab1",
+            image: UIImage(systemName: "pencil.and.outline"),
+            tag: 1
+        )
+        
+        let tab2 = UINavigationController()
+        tab2.viewControllers = [
+            Main_iosKt.MainViewController(),
+            Main_iosKt.MainViewController(),
+            Main_iosKt.MainViewController()
+        ]
+        tab2.tabBarItem = UITabBarItem(
+            title: "tab2",
+            image: UIImage(systemName: "pencil.and.outline"),
+            tag: 2
+        )
+        
+        tabs.viewControllers = [tab1, tab2]
+        
+        return tabs
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
@@ -18,4 +43,19 @@ struct ContentView: View {
 }
 
 
-
+class NativeUI: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        
+        let label = UILabel()
+        label.text = "Hello"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        view.addConstraints([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
